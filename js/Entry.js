58dbf44cf39c0e64de3ef73aef2dbe19bd05bc9d
Guest User <alexander.daniel.neumann@gmail.com>
2018-03-12 16:37:48 -0700

    Update allEntries.

--- js/Entry.js
+++ js/Entry.js
@@ -1,92 +1,81 @@
 //constructor for an Entry;
 export function Entry(inputTitle, inputBody) {
-  this.id = allEntries.length;
   this.title = inputTitle;
   this.body = inputBody;
   this.date = Date.now();
-};
+}
 //<----------------------------------CRUD METHODS------------------------------------>
-//CREATE
-Entry.prototype.CreateEntry = function(inputEntry){
-  allEntries.push(inputEntry);
-};
-//READ
-//Get entire list;
-Entry.prototype.GetAllEntries = function(){
-  return allEntries;
-};
-//Get entry at a particular index;
-Entry.prototype.GetEntryById = function(inputId){
-  const arrayId = findById(inputId);
-  return allEntries[arrayId];
-};
-//UPDATE
-Entry.prototype.UpdateEntryById = function(inputId, inputTitle, inputBody){
-  const arrayId = findById(inputId);
-  allEntries[arrayId].title = inputTitle;
-  allEntries[arrayId].body = inputBody;
-};
-//DESTROY
-Entry.prototype.DeleteEntryById = function(inputId){
-  const arrayId = findById(inputId);
-  Remove(arrayId);
-};
+// //CREATE
+// Entry.prototype.CreateEntry = function(inputEntry){
+//   allEntries.push(inputEntry);
+// };
+// //READ
+// //Get entire list;
+// Entry.prototype.GetAllEntries = function(){
+//   return allEntries;
+// };
+// //Get entry at a particular index;
+// Entry.prototype.GetEntryById = function(inputId){
+//   const arrayId = findById(inputId);
+//   return allEntries[arrayId];
+// };
+// //UPDATE
+// Entry.prototype.UpdateEntryById = function(inputId, inputTitle, inputBody){
+//   const arrayId = findById(inputId);
+//   allEntries[arrayId].title = inputTitle;
+//   allEntries[arrayId].body = inputBody;
+// };
+// //DESTROY
+// Entry.prototype.DeleteEntryById = function(inputId){
+//   const arrayId = findById(inputId);
+//   Remove(arrayId);
 //<--------------------------------END OF CRUD METHODS--------------------------------------->
 //<---------------------------------PROTOTYPE METHODS---------------------------------------->
 //Return number of words in entry
-Entry.prototype.GetWords = function(inputId){
-  const arrayId = findById(inputId);
+Entry.prototype.GetWords = function(inputBody){
   let wordCount = 0;
-
-  return allEntries[arrayId].body.length;
+  let operateOnMe = inputBody.split("");
+  for(var i = 0; i < operateOnMe.length; i ++){
+    wordCount++;
+  }
+  return wordCount;
 };
 //Return number of vowels
-Entry.prototype.GetVowels = function(inputId){
-  const arrayId = findById(inputId);
+Entry.prototype.GetVowels = function(inputBody){
   let vowelCount = 0;
-  let currentBody = allEntries[arrayId].body;
-  for(var i = 0; i < currentBody.length; i ++){
-    if(currentBody[i] == "a" || currentBody[i] == "i" || currentBody[i] == "e" || currentBody[i] == "o" || currentBody[i] == "u"){
+  let operateOnMe = inputBody.split("");
+  for(var i = 0; i < operateOnMe.length; i ++){
+    if(inputBody[i] == "a" || inputBody[i] == "i" || inputBody[i] == "e" || inputBody[i] == "o" || inputBody[i] == "u"){
       vowelCount ++;
     }
   }
   return vowelCount;
 };
-//Return first word of sentence
-Entry.prototype.FirstWord = function(inputId){
-  return FirstWordById(inputId, 1);
-};
-//Only display first 8 words
-Entry.prototype.FirstEight = function(inputId){
-  return FirstWordById(inputId, 8);
+//https://stackoverflow.com/questions/18558417/get-first-word-of-string
+Entry.prototype.GetWords = function(inputBody, numberOfWords){
+  var words = inputBody.split(" ");
+  var outputWords = [];
+  for(var i = 0; i < numberOfWords; i ++){
+    outputWords.push(words[i]);
+  }
+  return outputWords;
 };
 //<--------------------------------END OF PROTOTYPE METHODS--------------------------------------->
 //<------------------------------------HELPER METHODS--------------------------------------->
 //Reference: https://blog.mariusschulz.com/2016/07/16/removing-elements-from-javascript-arrays
-Entry.prototype.Remove = function(element){
-  const index = allEntries.indexOf(element);
-  if(index !== -1) {
-    array.splice(index,1);
-  }
-};
-Entry.prototype.FindById = function(inputId){
-  for(var i = 0; i < allEntries.length; i ++){
-    if(allEntries[i].id == inputId)
-    {
-      return inputId;
-    }
-  }
-  return -1;
-}
-//https://stackoverflow.com/questions/18558417/get-first-word-of-string
-Entry.prototype.GetWords = function(inputId, numberOfWords){
-  const arrayId = findById(inputId);
-  let currentBody = allEntries[arrayId].body;
-  var words = currentBody[i].split(" ");
-  var outputWords = [];
-  for(var i = 0; i < numberOfWords; i ++){
-    outputWords.push(words[numberOfWords]);
-  }
-  return outputWords;
-}
+// Entry.prototype.Remove = function(element){
+//   const index = allEntries.indexOf(element);
+//   if(index !== -1) {
+//     array.splice(index,1);
+//   }
+// };
+// Entry.prototype.FindById = function(inputId){
+//   for(var i = 0; i < allEntries.length; i ++){
+//     if(allEntries[i].id == inputId)
+//     {
+//       return inputId;
+//     }
+//   }
+//   return -1;
+// }
 //<------------------------------END OF HELPER METHODS--------------------------------------->
--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import '../css/styles.css';
-// import { Entry } from './../js/Entry.js';
-// var myEntries = [];
+import { Entry } from './../js/Entry.js';
+var allEntries = [];
+function displayAllEntries(){
+  $("#results").text("");
+  for(var i = 0; i < allEntries.length; i ++){
+    $("#results").append('<div class="col-md-2 result"> <h6> Entry Number: ' + i + ' </h6> <h2>Title:' + allEntries[i].title + '</h2><p> Body: ' + allEntries[i].body + '</p>');
+  }
+}
+$(document).ready(function(){
+  $("form#newEntry").submit(function(event) {
+    event.preventDefault();
+    var titleIn = $("#title").val();
+    var bodyIn = $("#body").val();
+    var newEntry = new Entry(titleIn, bodyIn);
+    allEntries.push(newEntry);
+  });
+  $("form#functionSelector").submit(function(event) {
+    event.preventDefault();
+    // var functionSelector = parseInt($("#functionSelector").val());
+  });
+  $("#refresh").click(function(){
+    console.log("Entered Refresh");
+    displayAllEntries();
+  });
+});
